17f6c4df1d78698eb7d0718b2999abee0a4f3134
Victor Hugo <victorhugobbpereira02@gmail.com>
2024-05-15 11:21:45 -0300

    Inicio da api com typescript

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = "Victor";
+let idade = 23;
+console.log(`Meu nome é ${nome}, tenho ${idade} anos`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = "Victor";
+let idade: number = 23;
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos`)
