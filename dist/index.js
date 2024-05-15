172e843f941916a0dd6491f4941355d9bf7ce8c0
Victor Hugo <victorhugobbpereira02@gmail.com>
2024-05-15 12:32:44 -0300

    Inicio da api com typescript

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
 let nome = "Victor";
 let idade = 23;
+for (let i = 0; i <= 3; i++) {
+    console.log("**##**");
+}
 console.log(`Meu nome é ${nome}, tenho ${idade} anos`);
+console.log(nome.length);
+console.log(Math.pow(idade, idade));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 let nome: string = "Victor";
 let idade: number = 23;
 
+for(let i: number = 0; i <= 3; i++){
+    console.log("**##**")
+}
+
 console.log(`Meu nome é ${nome}, tenho ${idade} anos`)
+console.log(nome.length)
+console.log(Math.pow(idade, idade))
